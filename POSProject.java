598da3b009014685a42c0a0897e40bbf2a01acad
RistelMari <157936693+RistelMari@users.noreply.github.com>
2024-01-28 11:25:45 +0800

    Update POSProject.java

---
+++ POSProject.java
@@ -0,0 +1,10 @@
+package com.mycompany.posproject;
+
+public class POSProject {
+
+    public static void main(String[] args) {
+        Product Opium = new Product(4.99, "Opium", 10);
+        Opium.purchaseItem();
+        System.out.println("The shop now has " + Opium.stock + " amount of " + Opium.name + " left!");
+    }
+}
